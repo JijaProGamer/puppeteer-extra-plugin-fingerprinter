3d93f13da271762a19b21805a349abcb4817423f
Bloxxy <jijaprogamer@gmail.com>
2025-03-22 09:21:27 +0200

    removed useless console.log

--- evasions/firefox/evasions/window.cjs
+++ evasions/firefox/evasions/window.cjs
@@ -1,5 +1,4 @@
 module.exports = async function (page, fingerprint) {
-    console.log(fingerprint.viewport)
     await page.setViewportSize({
         width: fingerprint.viewport.width,
         height: fingerprint.viewport.height
